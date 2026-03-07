df77d8dbdd4eeaec40e639b9dbcf71160fa8fc27
Marco Matos <marco.ufs@gmail.com>
2026-03-06 23:51:56 -0300

    Update uninstaller to remove files without confirmation and to delete the symlink pointing to the Steam dark-colored tray icon variant

--- uninstall.sh
+++ uninstall.sh
@@ -10,18 +10,21 @@ KSTEAMTRAYICON_DIR="$PREFIX/share/ksteamtrayicon"
 BIN_DIR="$PREFIX/bin"
 MAN_DIR="$PREFIX/share/man"
 AUTOSTART_DIR="/etc/xdg/autostart"
+PLASMA_ICON_DIR = "~/.local/share/icons"
 
-rm "$KSTEAMTRAYICON_DIR/dark-icon.png"
-rm "$KSTEAMTRAYICON_DIR/ksteamtrayicon.py"
-rm -r "$KSTEAMTRAYICON_DIR"
+rm -f "$KSTEAMTRAYICON_DIR/dark-icon.png"
+rm -f "$KSTEAMTRAYICON_DIR/ksteamtrayicon.py"
+rm -rf "$KSTEAMTRAYICON_DIR"
 
-rm "$BIN_DIR/ksteamtrayicon"
+rm -f "$BIN_DIR/ksteamtrayicon"
 
-rm "$AUTOSTART_DIR/ksteamtrayicon.desktop"
+rm -f "$AUTOSTART_DIR/ksteamtrayicon.desktop"
 
-rm "$MAN_DIR/man1/ksteamtrayicon.1.gz"
+rm -f "$MAN_DIR/man1/ksteamtrayicon.1.gz"
 
-rm "$MAN_DIR/pt_BR/man1/ksteamtrayicon.1.gz"
+rm -f "$MAN_DIR/pt_BR/man1/ksteamtrayicon.1.gz"
+
+rm -f "$PLASMA_ICON_DIR/steam_tray_mono.png"
 
 command -v mandb >/dev/null 2>&1 || true
 
